c402bed8760aef2fd668067b1ec8600835db24ea
Katya Kurchin <katyakurchin@gmail.com>
2023-10-04 16:47:41 -0400

    Updated server.js to add authenticate, login, error handling, and hash passwords

--- src/test/api/server.js
+++ src/test/api/server.js
@@ -6,8 +6,11 @@ AWS.config.update({
     region: 'us-east-2'
     accessKeyID: public key
     secretAccessKey: should not be hardcoded 
+
+    we can use aws configure to test, we can add the access keys to that in the terminal without writing in the code
 }); */
 
+const bcrypt = require('bcrypt');
 const { S3 } = require("@aws-sdk/client-s3");
 const gremlin = require('gremlin');
 const DriverRemoteConnection = gremlin.driver.DriverRemoteConnection;
@@ -17,8 +20,9 @@ const port = process.env.PORT || 8182;
 
 // Set up AWS credentials (2nd method), we can use this method for better security:
 //The AWS SDK will assign the EC2's temporary credentials from the assigned IAM role it has
+
 const s3 = new S3({
-    region: 'us-east-2',
+    //region: 'us-east-2',
 });
 
 s3.listBuckets(function(err, data) {
@@ -29,20 +33,15 @@ s3.listBuckets(function(err, data) {
     }
 });
 
-// Create drivers for reading and writing to database; each have their own endpoint and need their own driver/connection
-const readRemoteConnection = new gremlin.driver.DriverRemoteConnection({
-    remote: {
-        gremlin: 'wss://teamforgedb.cluster-ro-croj01onqdg2.us-east-2.neptune.amazonaws.com:8182/gremlin',
-    },
-    mimeType: 'application/vnd.gremlin-v2.0+json',
-});
+const readRemoteConnection = new gremlin.driver.DriverRemoteConnection(
+  'wss://teamforgedb.cluster-ro-croj01onqdg2.us-east-2.neptune.amazonaws.com:8182/gremlin',
+  { mimeType: 'application/vnd.gremlin-v2.0+json' }
+);
 
-const writeRemoteConnection = new gremlin.driver.DriverRemoteConnection({
-    remote: {
-        gremlin: 'wss://teamforgedb.cluster-croj01onqdg2.us-east-2.neptune.amazonaws.com',
-    },
-    mimeType: 'application/vnd.gremlin-v2.0+json'
-});
+const writeRemoteConnection = new gremlin.driver.DriverRemoteConnection(
+  'wss://teamforgedb.cluster-croj01onqdg2.us-east-2.neptune.amazonaws.com:8182/gremlin',
+  { mimeType: 'application/vnd.gremlin-v2.0+json' }
+);
 
 /*We can now query with either driver using Gremlin
 
@@ -59,51 +58,75 @@ app.use(express.json());
 
 
 //POST route to create a profile -- for the register page
-app.post('/api/createprofile', (req, res) => {
-  Username = 'TeamForge';
-  // Use some method to hash the password here
-  HashedPassword = 'servertest101';
+app.post('/api/createprofile', async (req, res) => {
+  const Username = 'TeamForge';
+  // Use some method to hash the password here, used bcrypt
+   const HashedPassword =  bcrypt.hashSync('servertest101', 10);
 
-  addProfile(Username, HashedPassword);
-  
-
-  res.status(201).json({message: 'Profile created successfully!'});
+   try {
+    await addProfile(Username, HashedPassword);
+    res.status(201).json({ message: 'Profile created successfully!' });
+} catch (error) {
+    console.error('Error creating profile:', error);
+    res.status(500).json({ message: 'Error creating profile. Please try again later.' });
+}
 });
 
-//GET route to sign in -- for the login page 
-app.get('/api/signin', (req, res) => {
-  Username = req.body.username;
-  //Hash the password here
-  HashedPassword = req.body.password;
 
-  // check the relational database to see if a user w/ the username exists and if it does, check that user's hashed pwd in the data
-  // --base with the hashed password sent in the request
 
-  if (Authenticate(Username, HashedPassword)){
-        // return the appropriate profile info
-  } else {
-      // return an error message stating the password was incorrect/did not match
+//POST route to sign in -- for the login page 
+app.post('/api/signin', async (req, res) => {
+  console.log("Signin route hit!");
+  const Username = req.body.username;
+  const plainTextPassword = req.body.password;
+  console.log("Entered plain-text password for login:", plainTextPassword);
+
+  try {
+      const storedHashedPassword = await getStoredHashedPassword(Username);
+      console.log("Stored hashed password for login:", storedHashedPassword);
+      if (storedHashedPassword && bcrypt.compareSync(plainTextPassword, storedHashedPassword)) {
+          res.status(200).json({ message: 'Successfully authenticated!' });
+      } else {
+          res.status(401).json({ message: 'Authentication failed. Check username and password.' });
+      }
+  } catch (error) {
+      console.error('Error during authentication:', error);
+      res.status(500).json({ message: 'Error during authentication. Please try again later.' });
   }
-
 });
 
 
 //query for creating profiles; still needs to check if the Username already exists
-function addProfile(Username, HashedPassword){
+async function addProfile(Username, HashedPassword){
   // use the write endpoint connection
-  graph1 = new Graph().traversal().withRemote(writeRemoteConnection); 
+ const graph1 = new Graph().traversal().withRemote(writeRemoteConnection); 
   // each node is a "user" node with the label as the username and properties that store the hashed password
-  graph1.addV(`${Username}`).property('Hashed_Password', HashedPassword).next()
-    .then(result => console.log(result))
-    .catch(error => console.log(error));
+  await graph1.addV(`${Username}`).property('Hashed_Password', HashedPassword).next()
+    
 
 }
 
 //query for authenticating a user in the database -- for the login page
-function Authenticate(Username, HashedPassword){
-  
+async function getStoredHashedPassword(Username) {
+  const graph = new Graph().traversal().withRemote(readRemoteConnection);
+  let userVertex;
+  try {
+      userVertex = await graph.V().hasLabel(Username).next();
+  } catch (error) {
+      console.error('Error querying the database:', error);
+      throw error;
+  }
+
+  if (userVertex && userVertex.value) {
+      return userVertex.value('Hashed_Password');
+  }
+  return null;
 }
 
+/* app.use((req, res, next) => {
+  res.status(404).send('Route not found');
+});
+*/
 // Server is running
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
