dce7b201ca738db2daab67bf4577f063dad79ec1
Henry Mitchell <henry.mitchell0903@gmail.com>
2023-10-03 23:09:19 -0400

    modified the login page

--- src/main/resources/components/LoginPage.tsx
+++ src/main/resources/components/LoginPage.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import { ChangeEvent } from "react";
+import { useNavigate } from "react-router-dom";
+import "../App.css";
 
 const userDatabase: [string, string][] = [
   ["hrmitch@emory.edu", "Password123"],
@@ -7,9 +9,16 @@ const userDatabase: [string, string][] = [
 ];
 
 function LoginPage() {
+  // Redirect
+  const navigate = useNavigate();
+  const redirectToRegisterPage = () => {
+    navigate("./RegisterPage");
+  };
+
   // State variables, hooks
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
+
   // Handlers
   const handleEmailChange = (e: ChangeEvent<HTMLInputElement>) => {
     setEmail(e.target.value);
@@ -30,24 +39,49 @@ function LoginPage() {
 
   return (
     <div className="login_page">
-      <h3>Login</h3>
-      <form>
-        <div className="form-group">
-          <label>Email:</label>
-          <input type="email" value={email} onChange={handleEmailChange} />
-        </div>
-        <div className="form-group">
-          <label>Password:</label>
-          <input
-            type="password"
-            value={password}
-            onChange={handlePasswordChange}
-          />
-        </div>
-        <button type="button" onClick={handleLogin}>
-          Login
+      <div className="circle"></div>
+      <div className="circle"></div>
+      <div className="login_box">
+        <h3>Login</h3>
+        <form className="login_form">
+          <div className="form-group">
+            <label>Email:</label>
+            <input
+              type="email"
+              value={email}
+              onChange={handleEmailChange}
+              onKeyDown={(e) => {
+                if (e.key === "Enter") {
+                  handleLogin();
+                }
+              }}
+            />
+          </div>
+          <div className="form-group">
+            <label>Password:</label>
+            <input
+              type="password"
+              value={password}
+              onChange={handlePasswordChange}
+              onKeyDown={(e) => {
+                if (e.key === "Enter") {
+                  handleLogin();
+                }
+              }}
+            />
+          </div>
+          <button className="login_button" type="button" onClick={handleLogin}>
+            Login
+          </button>
+        </form>
+        <button
+          className="register_button"
+          type="button"
+          onClick={redirectToRegisterPage}
+        >
+          Register
         </button>
-      </form>
+      </div>
     </div>
   );
 }
