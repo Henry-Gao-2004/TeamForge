30192d46d8b6ad1c0d7f56daa04ce784c7addc27
Henry Mitchell <henry.mitchell0903@gmail.com>
2023-10-17 22:51:40 -0400

    Updated login and registration pages. Lines that require HomePage.tsx are commented out.

--- src/main/resources/components/LoginPage.tsx
+++ src/main/resources/components/LoginPage.tsx
@@ -1,67 +1,157 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import jwt_decode from "jwt-decode";
 import { ChangeEvent } from "react";
-import { useNavigate  } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import "../App.css";
 
-
-let  userDatabase: [string, string][] = [
-    ["alexandra.iotzova@emory.edu", "Password123"],
+let userDatabase: [string, string][] = [
+  ["alexandra.iotzova@emory.edu", "Password123"],
+  ["ekurchin@emory.edu", "Password456"],
+  ["hrmitch@emory.edu", "Password789"],
 ];
 
 function LoginPage() {
-    // State variables, hooks
-    let navigate = useNavigate();
+  /* Google Authentication.*/
+  // User cache.
+  // useState gives bus the user object and a setter function.
+  const [user, setUser] = useState({});
 
-    const redirectToRegisterPage = () => {
-        navigate("/RegisterPage");
-    };
+  // Global Google.
+  function handleCallbackResponse(response) {
+    // response.credential is the JSON web token we recieve from login.
+    // response comes from documentation from google's id services.
+    console.log("Encoded JWT ID Token: " + response.credential);
 
-    const [email, setEmail] = useState<string>("");
-    const [password, setPassword] = useState<string>("");
-    // Handlers
-    const handleEmailChange = (e: ChangeEvent<HTMLInputElement>) => {
-        setEmail(e.target.value);
-    };
-    const handlePasswordChange = (e: ChangeEvent<HTMLInputElement>) => {
-        setPassword(e.target.value);
-    };
-    const handleLogin = () => {
-        const confirmUser = userDatabase.find(([username, userPassword]) => {
-            return username === email && userPassword === password;
-        });
-        if (confirmUser) {
-            alert("Login successful");
-        } else {
-            alert("Invalid username or password");
+    var userObject = jwt_decode(response.credential);
+    console.log(userObject);
+    setUser(userObject);
 
-        }
-    };
+    // Hide the sign in button when we log in.
+    document.getElementById("signInDiv").hidden = true;
+  }
 
-    return (
-        <div className="login_page" id = "login">
-            <h3>Login</h3>
-            <form>
-                <div className="form-group">
-                    <label>Email:</label>
-                    <input type="email" value={email} onChange={handleEmailChange} />
-                </div>
-                <div className="form-group">
-                    <label>Password:</label>
-                    <input
-                        type="password"
-                        value={password}
-                        onChange={handlePasswordChange}
-                    />
-                </div>
-                <button type="button" onClick={handleLogin}>
-                    Login
-                </button>
-                <button type="button" onClick={redirectToRegisterPage}>
-                    Register
-                </button>
-            </form>
+  function handleSignOut(event) {
+    // clear user object
+    setUser({});
+    document.getElementById("signInDiv").hidden = false;
+  }
+
+  useEffect(() => {
+    google.accounts.id.initialize({
+      client_id:
+        "633951300634-vu1l1cnpnmuuls39nteir5qr0s1rj4dj.apps.googleusercontent.com",
+      callback: handleCallbackResponse,
+    });
+    google.accounts.id.renderButton(document.getElementById("signInDiv"), {
+      theme: "outline",
+      size: "large",
+    });
+
+    // Google's "1-tap dialogue" to log in.
+    // prompt (pop-up) shows accounts that you recently logged in with for easier access.
+    // prompt still activates the same callback response.
+  }, []);
+
+  // Redirects.
+  const navigate = useNavigate();
+  const redirectToRegisterPage = () => {
+    navigate("./RegisterPage");
+  };
+
+  // State variables, hooks.
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  // Handlers.
+  const handleEmailChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+  };
+  const handlePasswordChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+  };
+  const handleLogin = () => {
+    const confirmUser = userDatabase.find(([username, userPassword]) => {
+      return username === email && userPassword === password;
+    });
+    if (confirmUser) {
+      navigate("/HomePage"), { state: { email: email } };
+    } else {
+      alert("Invalid username or password");
+    }
+  };
+
+  return (
+    <div className="login_box" id="login">
+      <div className="login_header">
+        <span className="blue_text">Team</span>Forge
+      </div>
+      <form className="login_form">
+        <div className="subtitle">Login</div>
+        <div className="form-group">
+          <input
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={handleEmailChange}
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
+                handleLogin();
+              }
+            }}
+          />
         </div>
-    );
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={handlePasswordChange}
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
+                handleLogin();
+              }
+            }}
+          />
+        </div>
+        <footer>
+          <div className="login_bottom_left" id="signInDiv">
+            Login with Google.
+          </div>
+          {
+            //to show signout button when logged in
+
+            //P && Q: if P is true do Q
+            Object.keys(user).length != 0 && (
+              <button onClick={(e) => handleSignOut(e)}>Sign Out</button>
+            )
+          }
+          {user && (
+            <div>
+              {/* once signed in, we display the username and profile picture */}
+              <img src={user.picture}></img>
+              <h3>{user.name}</h3>
+            </div>
+          )}
+          <div className="login_bottom_right">
+            <button
+              className="login_button"
+              type="button"
+              onClick={handleLogin}
+            >
+              Login
+            </button>
+            <button
+              className="login_redirect_button"
+              type="button"
+              onClick={redirectToRegisterPage}
+            >
+              Register
+            </button>
+          </div>
+        </footer>
+      </form>
+    </div>
+  );
 }
 
 export default LoginPage;
-export const uDB  :  [string, string][] = userDatabase
--- src/main/resources/components/RegisterPage.tsx
+++ src/main/resources/components/RegisterPage.tsx
@@ -1,87 +1,109 @@
 import React, { useState, ChangeEvent } from "react";
 import { useNavigate } from "react-router-dom";
-import {uDB} from "./LoginPage";
+import "../App.css";
 
-
-//const userDatabase: [string, string][] = [
-//   ["alexandra.iotzova@emory.edu", "Password123"]
-//];
+const userDatabase: [string, string][] = [
+  ["alexandra.iotzova@emory.edu", "Password123"],
+];
 
 function RegistrationPage() {
-    const [email, setEmail] = useState<string>("");
-    const [password, setPassword] = useState<string>("");
-    const [confirmPassword, setConfirmPassword] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [confirmPassword, setConfirmPassword] = useState<string>("");
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const redirectToLoginPage = () => {
-        navigate("/");
-    };
+  const redirectToLoginPage = () => {
+    navigate("/");
+  };
 
-    const handleEmailChange = (e: ChangeEvent<HTMLInputElement>) => {
-        setEmail(e.target.value);
-    };
+  const handleEmailChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+  };
 
-    const handlePasswordChange = (e: ChangeEvent<HTMLInputElement>) => {
-        setPassword(e.target.value);
-    };
+  const handlePasswordChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+  };
 
-    const handleConfirmPasswordChange = (e: ChangeEvent<HTMLInputElement>) => {
-        setConfirmPassword(e.target.value);
-    };
+  const handleConfirmPasswordChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setConfirmPassword(e.target.value);
+  };
 
-    const handleRegistration = () => {
-        console.log("Email:", email);
-        console.log("Password:", password);
-        console.log("Confirm Password:", confirmPassword);
-        if (password != confirmPassword ) {
-            alert("Passwords do not match")
-        }
-        const confirmUser = uDB.find(([username, userPassword]) => {
-            return username === email && userPassword === password;
-        });
-        if (confirmUser) {
-            alert("Login already  there");
-        } else {
-            alert("Creating");
+  const handleRegistration = () => {
+    console.log("Email:", email);
+    console.log("Password:", password);
+    console.log("Confirm Password:", confirmPassword);
+    if (password != confirmPassword) {
+      alert("Passwords do not match");
+    }
+    const confirmUser = userDatabase.find(([username, userPassword]) => {
+      return username === email && userPassword === password;
+    });
+    if (confirmUser) {
+      alert("Login already exists");
+    } else {
+      alert("Creating");
+    }
+  };
 
-        }
-
-    };
-
-    return (
-        <div className="registration_page">
-            <h3>Register</h3>
-            <form>
-                <div className="form-group">
-                    <label>Email:</label>
-                    <input type="email" value={email} onChange={handleEmailChange} />
-                </div>
-                <div className="form-group">
-                    <label>Password:</label>
-                    <input type="password" value={password} onChange={handlePasswordChange} />
-                </div>
-                <div className="form-group">
-                    <label>Confirm Password:</label>
-                    <input
-                        type="password"
-                        value={confirmPassword}
-                        onChange={handleConfirmPasswordChange}
-                    />
-                </div>
-                <button type="button" onClick={handleRegistration}>
-                    Register
-                </button>
-                <button
-                    className="login_page"
-                    type="button"
-                    onClick={redirectToLoginPage}
-                >
-                    Login
-                </button>
-            </form>
+  return (
+    <div className="register_page" id="register">
+      <div className="login_box">
+        <div className="login_header">
+          <span className="blue_text">Team</span>Forge
         </div>
-    );
+        <form className="login_form">
+          <div className="subtitle">Register</div>
+          <div className="form-group">
+            <input
+              type="email"
+              placeholder="Email"
+              value={email}
+              onChange={handleEmailChange}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={handlePasswordChange}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              placeholder="Confirm password"
+              value={confirmPassword}
+              onChange={handleConfirmPasswordChange}
+              onKeyDown={(e) => {
+                if (e.key === "Enter") {
+                  handleRegistration();
+                }
+              }}
+            />
+          </div>
+          <footer>
+            <div className="login_bottom_left">
+              Using Google? Return to Login.
+            </div>
+            <div className="login_bottom_right">
+              <button
+                className="login_redirect_button"
+                type="button"
+                onClick={redirectToLoginPage}
+              >
+                Login
+              </button>
+              <button type="button" onClick={handleRegistration}>
+                Register
+              </button>
+            </div>
+          </footer>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default RegistrationPage;
